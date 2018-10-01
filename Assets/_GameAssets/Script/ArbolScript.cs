f91d2fe57a1d357c1918a24ae327bcb7d90983c3
sagounitydev <sago.unitydev@gmail.com>
2018-10-01 14:00:03 +0200

    Initial commit

---
+++ Assets/_GameAssets/Script/ArbolScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArbolScript : MonoBehaviour {
+
+    AudioSource audioSource;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        audioSource.Play();
+        Invoke("DestruirObjeto", 0.032f);
+    }
+
+    void DestruirObjeto()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/_GameAssets/Script/SaltaPollo.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaltaPollo : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    [SerializeField] int fuerza = 100;
+    [SerializeField] Transform posPies;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("s"))
+        {
+            Collider[] cols = Physics.OverlapSphere(posPies.position, 10);
+            for (int i=0; i <cols.Length; i++)
+            {
+                Debug.Log("ESTOY TOCANDO UN" + i);
+            }
+
+            rb.AddForce(new Vector3 (1,1,0) * fuerza);
+        }
+        else if (Input.GetKeyDown("w"))
+        {
+            Collider[] cols = Physics.OverlapSphere(posPies.position, 1f, LayerMask.NameToLayer("Terreno"));
+            for (int i = 0; i < cols.Length; i++)
+            {
+                Debug.Log("ESTOY TOCANDO UN" + i);
+            }
+
+            rb.AddForce(new Vector3(-1, 1, 0) * fuerza);
+        }
+        else if (Input.GetKeyDown("d"))
+        {
+            Collider[] cols = Physics.OverlapSphere(posPies.position, 10);
+            for (int i = 0; i < cols.Length; i++)
+            {
+                Debug.Log("ESTOY TOCANDO UN" + i);
+            }
+
+            rb.AddForce(new Vector3(0, 1, 1) * fuerza);
+        }
+        else if (Input.GetKeyDown("a"))
+        {
+            Collider[] cols = Physics.OverlapSphere(posPies.position, 10);
+            for (int i = 0; i < cols.Length; i++)
+            {
+                Debug.Log("ESTOY TOCANDO UN" + i);
+            }
+
+            rb.AddForce(new Vector3(0, 1, -1) * fuerza);
+        }
+    }
+}
